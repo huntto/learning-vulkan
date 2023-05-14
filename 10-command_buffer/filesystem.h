bd6d0fc72151cde3d4ca05ed369a66de9a185167
huntto <huntto1990@icloud.com>
2023-05-14 21:09:26 +0800

    create command pool and buffer

---
+++ 10-command_buffer/filesystem.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <fstream>
+#include <vector>
+
+std::vector<char> ReadFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+    if (!file.is_open()) {
+        throw std::runtime_error("failed to open file!");
+    }
+
+    size_t file_size = (size_t)file.tellg();
+    std::vector<char> buffer(file_size);
+
+    file.seekg(0);
+    file.read(buffer.data(), file_size);
+    file.close();
+
+    return buffer;
+}
